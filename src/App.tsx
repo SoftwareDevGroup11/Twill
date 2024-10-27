a3840d5b983ee9c8df80a3e961337be7596e82f6
Varunbalaji06 <varunbalaji06@gmail.com>
2024-10-27 14:00:09 +0530

    Enabled New File Button..

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,183 @@
-import './App.css';
-import TextEditor from './components/texteditor.tsx';
+import "./App.css";
+import TextEditor from "./components/texteditor.tsx";
+import { useState } from "react";
 
 function App() {
-    return (
-        <>
-            <nav className="navbar">
-                <ul className="menu">
-                    <li className="menu-item">
-                        File
-                        <ul className="dropdown">
-                            <li>New File</li>
-                            <li>New Window</li>
-                            <li>Open File</li>
-                            <li>Save</li>
-                            <li>Save As</li>
-                            <li>Share</li>
-                            <li>Exit</li>
-                        </ul>
-                    </li>
-                    <li className="menu-item">
-                        Edit
-                        <ul className="dropdown">
-                            <li>Undo</li>
-                            <li>Redo</li>
-                            <li>Cut</li>
-                            <li>Copy</li>
-                            <li>Paste</li>
-                        </ul>
-                    </li>
-                    <li className="menu-item">
-                        View
-                        <ul className="dropdown">
-                            <li>Preview</li>
-                            <li>Full Screen</li>
-                        </ul>
-                    </li>
-                    <li className="menu-item">
-                        Format
-                        <ul className="dropdown">
-                            <li>Bold</li>
-                            <li>Italic</li>
-                            <li>Underline</li>
-                            <li>Font</li>
-                            <li>Font Size</li>
-                            <li>Align</li>
-                            <li>Heading</li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-            <TextEditor />
-        </>
-    );
+  const [files, setFiles] = useState([
+    { id: 1, name: "Untitled", content: "" },
+  ]);
+  const [currentFileIndex, setCurrentFileIndex] = useState(0);
+  const [isRenaming, setIsRenaming] = useState(false);
+  const [newFileName, setNewFileName] = useState("");
+
+  const addNewFile = () => {
+    const newFile = {
+      id: files.length + 1,
+      name: `Untitled-${files.length + 1}`,
+      content: "",
+    };
+    setFiles([...files, newFile]);
+    setCurrentFileIndex(files.length);
+  };
+
+  const handleTabClick = (index: number) => {
+    setCurrentFileIndex(index);
+    setIsRenaming(false);
+  };
+
+  const handleNameChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const updatedFiles = [...files];
+    updatedFiles[index].name = e.target.value;
+    setFiles(updatedFiles);
+  };
+
+  const removeFile = (index: number) => {
+    const updatedFiles = files.filter((_, i) => i !== index);
+    setFiles(updatedFiles);
+    if (index === currentFileIndex) {
+      setCurrentFileIndex(index > 0 ? index - 1 : 0);
+    } else if (index < currentFileIndex) {
+      setCurrentFileIndex(currentFileIndex - 1);
+    }
+    setIsRenaming(false);
+  };
+
+  const startEditing = (index: number) => {
+    setIsRenaming(true);
+    setNewFileName(files[index].name);
+  };
+
+  const handleRenameClick = () => {
+    startEditing(currentFileIndex);
+  };
+
+  const handleRenameSubmit = () => {
+    const updatedFiles = [...files];
+    updatedFiles[currentFileIndex].name = newFileName;
+    setFiles(updatedFiles);
+    setIsRenaming(false);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="menu">
+          <li className="menu-item">
+            File
+            <ul className="dropdown">
+              <li onClick={addNewFile}>New File</li>
+
+              <li>New Window</li>
+
+              <li onClick={handleRenameClick}>Rename</li>
+
+              <li>Open File</li>
+
+              <li>Save</li>
+
+              <li>Save As</li>
+
+              <li>Share</li>
+
+              <li>Exit</li>
+            </ul>
+          </li>
+
+          <li className="menu-item">
+            Edit
+            <ul className="dropdown">
+              <li>Undo</li>
+
+              <li>Redo</li>
+
+              <li>Cut</li>
+
+              <li>Copy</li>
+
+              <li>Paste</li>
+            </ul>
+          </li>
+
+          <li className="menu-item">
+            View
+            <ul className="dropdown">
+              <li>Preview</li>
+
+              <li>Full Screen</li>
+            </ul>
+          </li>
+
+          <li className="menu-item">
+            Format
+            <ul className="dropdown">
+              <li>Bold</li>
+
+              <li>Italic</li>
+
+              <li>Underline</li>
+
+              <li>Font</li>
+
+              <li>Font Size</li>
+
+              <li>Align</li>
+
+              <li>Heading</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <div className="file-tab-bar">
+        {files.map((file, index) => (
+          <div
+            key={file.id}
+            className={`file-tab ${index === currentFileIndex ? "active" : ""}`}
+            onClick={() => handleTabClick(index)}
+          >
+            {isRenaming && index === currentFileIndex ? (
+              <input
+                type="text"
+                value={newFileName}
+                onChange={(e) => setNewFileName(e.target.value)}
+                onBlur={handleRenameSubmit}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleRenameSubmit();
+                  }
+                }}
+              />
+            ) : (
+              <span onClick={() => startEditing(index)}>{file.name}</span>
+            )}
+            <span
+              className="close-btn"
+              onClick={(e) => {
+                e.stopPropagation();
+                removeFile(index);
+              }}
+            >
+              x
+            </span>
+          </div>
+        ))}
+      </div>
+      {files.length > 0 ? (
+        <TextEditor
+          file={files[currentFileIndex]}
+          updateFileContent={(content: string) => {
+            const updatedFiles = [...files];
+            updatedFiles[currentFileIndex].content = content;
+            setFiles(updatedFiles);
+          }}
+        />
+      ) : (
+        <div className="no-file">No files open</div>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/components/texteditor.tsx
+++ src/components/texteditor.tsx
@@ -1,91 +1,98 @@
-import { FileOBJ } from '../texteditor/fileobj';
-import './texteditor.css';
+import { FileOBJ } from "../texteditor/fileobj";
+import "./texteditor.css";
 
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 
 const TextEditor = () => {
-    let file = new FileOBJ();
-    file.parse("#include <iostream>\n\nint main() {\n    std::cout << \"Hello World\" << std::endl;\n}");
+  let file = new FileOBJ();
+  file.parse(
+    '\n\n#include <iostream>\n\nint main() {\n    std::cout << "Hello World" << std::endl;\n}'
+  );
 
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    function draw() {
-	const canvas = canvasRef.current;
-	if (!canvas) return;
-	const ctx = canvas.getContext("2d");
+  function draw() {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
 
-	if (!ctx) return;
+    if (!ctx) return;
 
-	ctx.fillStyle = '#000000' // Background Color
-	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = "#000000"; // Background Color
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-	ctx.fillStyle = '#ffffff'
+    ctx.fillStyle = "#ffffff";
 
-	// Font Should be a Monospace font, or else it will break the cursor
-	ctx.font = "20px courier"; 
+    // Font Should be a Monospace font, or else it will break the cursor
+    ctx.font = "20px courier";
 
-	for (let j = 0; j < file.lines.length; j++) {
-	    let text = file.lines[j].dump();
-	    for (let i = 0; i < text.length; i++) {
-		ctx.fillText(text[i], i * 12, 20 + 20 * j);
-	    }
-	}
-
-	// Drawing the cursor
-	ctx.fillRect(file.lines[file.currentLine].left * 12, file.currentLine * 20 + 5, 1, 20);
+    for (let j = 0; j < file.lines.length; j++) {
+      let text = file.lines[j].dump();
+      for (let i = 0; i < text.length; i++) {
+        ctx.fillText(text[i], i * 12, 20 + 20 * j);
+      }
     }
 
-    useEffect(() => {
-	draw();
-	const canvas = canvasRef.current;
-	if (!canvas) return;
-	canvas.focus();
-    }, [file.lines]);
+    // Drawing the cursor
+    ctx.fillRect(
+      file.lines[file.currentLine].left * 12,
+      file.currentLine * 20 + 5,
+      1,
+      20
+    );
+  }
 
-    // TODO: Make this more clean. Add a better abstraction
-    function onInputHandle(event : React.KeyboardEvent<HTMLCanvasElement>) {
-	event.preventDefault();
-	if (event.key.length === 1) {
-	    file.insertText(event.key);
-	    draw();
-	} else if (event.key === "ArrowUp") {
-	    file.moveCursorUpBy(1);
-	    draw();
-	} else if (event.key === "ArrowDown") {
-	    file.moveCursorDownBy(1);
-	    draw();
-	} else if (event.key === "ArrowLeft") {
-	    file.moveCursorLeftBy(1);
-	    draw();
-        } else if (event.key === "ArrowRight") {
-	    file.moveCursorRightBy(1);
-	    draw();
-        } else if (event.key === "Backspace") {
-	    file.backspace();
-	    draw();
-        } else if (event.key === "Delete") {
-	    file.delete();
-	    draw();
-        } else if (event.key === "Enter") {
-	    file.insertNewline();
-	    draw();
-        } else if(event.key === "Tab"){
-		file.insertText("				"); // added a very naive implementation of TAB, using movecursorRightby 4 doesn't provide intended output
-		draw();
-	}
+  useEffect(() => {
+    draw();
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvas.focus();
+  }, [file.lines]);
+
+  // TODO: Make this more clean. Add a better abstraction
+  function onInputHandle(event: React.KeyboardEvent<HTMLCanvasElement>) {
+    event.preventDefault();
+    if (event.key.length === 1) {
+      file.insertText(event.key);
+      draw();
+    } else if (event.key === "ArrowUp") {
+      file.moveCursorUpBy(1);
+      draw();
+    } else if (event.key === "ArrowDown") {
+      file.moveCursorDownBy(1);
+      draw();
+    } else if (event.key === "ArrowLeft") {
+      file.moveCursorLeftBy(1);
+      draw();
+    } else if (event.key === "ArrowRight") {
+      file.moveCursorRightBy(1);
+      draw();
+    } else if (event.key === "Backspace") {
+      file.backspace();
+      draw();
+    } else if (event.key === "Delete") {
+      file.delete();
+      draw();
+    } else if (event.key === "Enter") {
+      file.insertNewline();
+      draw();
+    } else if (event.key === "Tab") {
+      file.insertText("				"); // added a very naive implementation of TAB, using movecursorRightby 4 doesn't provide intended output
+      draw();
     }
+  }
 
-    return (
-	<canvas 
-	    id="canvas" 
-	    ref={canvasRef} 
-	    width="1280" 
-	    height="720" 
-	    tabIndex={0}
-	    onKeyDown={onInputHandle}
-	    onClick={() => canvasRef.current?.focus()}
-	></canvas>
-    )
-}
+  return (
+    <canvas
+      id="canvas"
+      ref={canvasRef}
+      width="1280"
+      height="720"
+      tabIndex={0}
+      onKeyDown={onInputHandle}
+      onClick={() => canvasRef.current?.focus()}
+    ></canvas>
+  );
+};
 
 export default TextEditor;
